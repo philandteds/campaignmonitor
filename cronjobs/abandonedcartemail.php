8c584603149b7ea8bbe7b67321d9bef861236b4d
mfh <mark@clearfield.com>
2016-05-31 12:16:21 +1200

    Boilerplate code (cronjob, initial ini settings, design directory with test email)

---
+++ cronjobs/abandonedcartemail.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mfh
+ * Date: 31/05/16
+ * Time: 11:57 AM
+ */
---
+++ settings/cronjob.ini.append.php
@@ -0,0 +1,9 @@
+<?php /* #?ini charset="utf-8"?
+
+[CronjobSettings]
+ScriptDirectories[]=extension/ax_import/cronjobs
+Scripts[]=remove_expired_ax_import_logs.php
+
+[CronjobPart-frequent]
+Scripts[]=remove_expired_ax_import_logs.php
+*/ ?>
