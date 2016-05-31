86b3fc196c00d8d2ebdef61b6c38fb8f05e74df4
mfh <mark@clearfield.com>
2016-05-31 16:25:43 +1200

    Parametized the date range (with defaults set in the code) Refactored identifyAbandonedCarts() for testability

--- cronjobs/abandonedcartemail.php
+++ cronjobs/abandonedcartemail.php
@@ -7,8 +7,15 @@
 
 DEFINE ('SECONDS_IN_DAY', 60*60*24);
 
-$dateRangeStart = 50;
-$dateRangeEnd = 40;
+$ini = eZINI::instance('campaign_monitor.ini');
+$dateRangeStart = $ini->variable('AbandonedCartEmail', 'DateRangeStartDays');
+$dateRangeEnd = $ini->variable('AbandonedCartEmail', 'DateRangeEndDays');
+
+// default values
+if (!$dateRangeStart)
+    $dateRangeStart = 7;
+if (!$dateRangeEnd)
+    $dateRangeEnd = 1;
 
 
 $cli = eZCLI::instance();
@@ -21,40 +28,66 @@ $toDate = time() - ($dateRangeEnd * SECONDS_IN_DAY);
 
 $db = eZDB::instance();
 
-// To identify whether a cart has been abandoned, we must identify temporary carts (is_temporary = 0) with no corresponding later
-// non-temporary ones. Unfortunately, the email address is buried in an XML field, and a pure SQL solution is extremely slow without
-// the means to do an efficient join. Therefore, we handle the exclusion of completed carts in code by iterating through all
-// carts, adding those that are temporary and removing those that are not. The dataset is sorted so that non-temporary carts
-// always come after temporary ones.
-$sql = "
-    select max(created) as created, is_temporary, substring(data_text_1, locate('<email>', data_text_1) + length('<email>'), 
-                 locate('</email>', data_text_1)-locate('<email>', data_text_1)-length('<email>')) as email
-    from ezorder
-    where created > $fromDate
-    group by is_temporary, email
-    order by email, is_temporary
-";
-
-$rows = $db->arrayQuery($sql);
-$candidateCarts = array();
-
-foreach ($rows as $row) {
-    $is_temporary = $row['is_temporary'];
-    $created = $row['created'];
-    $email = $row['email'];
-
-    if ($is_temporary == 1 && $created > $fromDate && $created <= $toDate) {
-        $candidateCarts[$email] = 1;
-    } else if ($is_temporary == 0) {
-        unset($candidateCarts[$email]);
-    }
-}
+$abandonedCarts = identifyAbandonedCarts($db, $fromDate, $toDate);
 
 // we've passed through the data. Anything left in the $candidateCarts array is fair game
-foreach (array_keys($candidateCarts) as $cartEmail) {
+foreach ($abandonedCarts as $cartEmail) {
     $cli->notice("Abandoned cart detected: $cartEmail");
 }
 
 if (!$cli->isQuiet()) {
     $cli->notice("Finished Abandoned Cart Email Processing");
+}
+
+
+/**
+ * Find the abandoned carts (within a date range)
+ *
+ * To identify whether a cart has been abandoned, we must identify temporary carts (is_temporary = 0) with no corresponding later
+ * non-temporary ones. Unfortunately, the email address is buried in an XML field, and a pure SQL solution is extremely slow without
+ * the means to do an efficient join. Therefore, we handle the exclusion of completed carts in code by iterating through all
+ * carts, adding those that are temporary and removing those that are not. The dataset is sorted so that non-temporary carts
+ * always come after temporary ones.
+ *
+ * @param $db Database instance
+ * @param $fromDate long start of date range to check for abandoned carts (ie the earliest date/time)
+ * @param $toDate long end of date range to check for abandoned carts (ie the latest date/time)
+ * @return array List of the email addresses with abandoned carts
+ */
+function identifyAbandonedCarts($db, $fromDate, $toDate) {
+    $sql = "
+    select max(created) as created, is_temporary, substring(data_text_1, locate('<email>', data_text_1) + length('<email>'), 
+                 locate('</email>', data_text_1)-locate('<email>', data_text_1)-length('<email>')) as email
+    from ezorder
+    where created > $fromDate
+    group by is_temporary, email
+    order by email, is_temporary desc
+";
+
+    $rows = $db->arrayQuery($sql);
+    $candidateCarts = array();
+
+    foreach ($rows as $row) {
+        $is_temporary = $row['is_temporary'];
+        $created = $row['created'];
+        $email = strtolower($row['email']);
+
+        if ($is_temporary == 1 && $created > $fromDate && $created <= $toDate) {
+            $candidateCarts[$email] = 1;
+        } else if ($is_temporary == 0) {
+            unset($candidateCarts[$email]);
+        }
+    }
+
+    // we've passed through the data. Anything left in the $candidateCarts array is fair game
+    return array_keys($candidateCarts);
+}
+
+/**
+ * Sends the abandoned cart email through the Campaign Monitor interface
+ *
+ * @param $email String the recipient of the email
+ */
+function sendAbandonedCartEmail($email) {
+    
 }
